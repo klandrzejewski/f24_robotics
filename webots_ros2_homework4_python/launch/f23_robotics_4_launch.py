571360e70dc905d9da965821695bf9c7caf410a2
klandrzejewski <klandrzejewski@crimson.ua.edu>
2024-12-06 17:12:03 -0600

    update world

--- webots_ros2_homework4_python/launch/f23_robotics_4_launch.py
+++ webots_ros2_homework4_python/launch/f23_robotics_4_launch.py
@@ -142,7 +142,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'world',
-            default_value='FirstFloorStudentCenter.wbt',
+            default_value='maze1_smallest.wbt',
             description='Choose one of the world files from `/webots_ros2_turtlebot/world` directory'
         ),
         DeclareLaunchArgument(
--- webots_ros2_homework4_python/setup.py
+++ webots_ros2_homework4_python/setup.py
@@ -7,7 +7,7 @@ data_files.append(('share/ament_index/resource_index/packages', ['resource/' + p
 data_files.append(('share/' + package_name + '/launch', ['launch/f23_robotics_4_launch.py']))
 
 data_files.append(('share/' + package_name + '/worlds', [
-    'worlds/FirstFloorStudentCenter.wbt', 
+    'worlds/maze1_smallest.wbt', 
 ]))
 data_files.append(('share/' + package_name, ['package.xml']))
 data_files.append(('share/' + package_name + '/resource', [
