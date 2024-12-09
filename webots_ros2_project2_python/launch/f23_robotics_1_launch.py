97b2e3a4d74b716230230df8ff58784fe1e47b44
klandrzejewski <klandrzejewski@crimson.ua.edu>
2024-12-08 20:02:26 -0600

    update world

--- webots_ros2_project2_python/launch/f23_robotics_1_launch.py
+++ webots_ros2_project2_python/launch/f23_robotics_1_launch.py
@@ -104,7 +104,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'world',
-            default_value='maze.wbt',
+            default_value='maze1_smallest.wbt',
             description='Choose one of the world files from `/webots_ros2_turtlebot/world` directory'
         ),
         DeclareLaunchArgument(
